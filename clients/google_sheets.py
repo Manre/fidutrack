3a0e39ec93ff6f5e116289ea19ff5bfaac556f7a
Ernesto Nobmann <nobmann88@gmail.com>
2023-04-11 22:44:24 -0500

    update credentials for google sheets

--- clients/google_sheets.py
+++ clients/google_sheets.py
@@ -1,8 +1,4 @@
-import os
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -20,19 +16,7 @@ class GoogleSheets(BaseClient):
         self.spreadsheet_id = spreadsheet_id
 
     def authenticate(self):
-        creds = None
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                print('if creds...')
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
-                creds = flow.run_local_server(port=0)
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
-
+        creds = Credentials.from_service_account_file('credentials/prod.json', scopes=self.SCOPES)
         self.service = build('sheets', 'v4', credentials=creds)
 
     def add(
